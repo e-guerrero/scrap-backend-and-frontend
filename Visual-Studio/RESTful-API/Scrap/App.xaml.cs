fe92e41a9d3f0e64130a91cf466442e5d67cc6f7
edwinguerrerotech <53377253+edwinguerrerotech@users.noreply.github.com>
2019-11-23 01:39:44 -0500

    Add files via upload

---
+++ Visual-Studio/RESTful-API/Scrap/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Scrap.ViewModels;
+using Scrap.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Scrap
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ReaderViewModel vm = new ViewModels.ReaderViewModel();
+            MainWindow win = new MainWindow();
+            win.DataContext = vm;
+            win.Show();
+
+        }
+    }
+}
