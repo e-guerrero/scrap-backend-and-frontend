2f59181e8fcb0f02fa9869add75b75993b2f6dd7
edwinguerrerotech <53377253+edwinguerrerotech@users.noreply.github.com>
2019-11-23 01:28:55 -0500

    Add files via upload

---
+++ Visual-Studio/RESTful-API/Model/GeneralModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Model
+{
+    public class Wrapper<T>
+    {
+        public List<T> Data { get; set; }
+    }
+    [DataContract]
+    public class DTO_Manufacture
+    {
+        [DataMember]
+        public int manID { get; set; }
+        [DataMember]
+        public string manName { get; set; }
+    }
+
+    public class DTO_Part
+    {
+        [DataMember]
+        public int partsID { get; set; }
+        [DataMember]
+        public int manID { get; set; }
+        [DataMember]
+        public int modelID { get; set; }
+        [DataMember]
+        public string partNum { get; set; }
+        [DataMember]
+        public string partDesc { get; set; }
+        [DataMember]
+        public string partPic { get; set; }
+        public DTO_Manufacture manufacture { get; set; }
+
+    }
+}
